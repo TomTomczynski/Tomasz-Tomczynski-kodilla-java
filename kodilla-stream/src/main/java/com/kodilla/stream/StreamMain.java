fe9b5bc0915d0c3abe90d01178aeb48aa2f043f4
TomTomczynski <tom.tomczynski@gmail.com>
2019-07-02 18:23:30 +0200

    ForumUser class sub 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -16,21 +17,23 @@ public class StreamMain {
 //        Executor codeToExecute = () -> System.out.println("This is lambda.");
 //        processor.execute(() -> System.out.println("This is lambda."));
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 //        expressionExecutor.executeExpression(10, 5, (a1, b1) -> a1 + b1);
 //        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
 //        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
 //        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
 
-        PoemBeautifier thePoem = new PoemBeautifier();
-        String theText = "This is Thom's text for testing.";
-        thePoem.beautify(theText, (String) -> "!!!! " + theText + " !!!!" + '\n');
-        thePoem.beautify(theText, (String) -> theText.toLowerCase() + '\n');
-        thePoem.beautify(theText, (String) -> theText.toUpperCase() + '\n');
-        thePoem.beautify(theText, (String) -> {
-            StringBuilder theStrigBuilder = new StringBuilder(theText);
-            return (theStrigBuilder.reverse().toString());
-        });
+//        PoemBeautifier thePoem = new PoemBeautifier();
+//        String theText = "This is Thom's text for testing.";
+//        thePoem.beautify(theText, (String) -> "!!!! " + theText + " !!!!" + '\n');
+//        thePoem.beautify(theText, (String) -> theText.toLowerCase() + '\n');
+//        thePoem.beautify(theText, (String) -> theText.toUpperCase() + '\n');
+//        thePoem.beautify(theText, (String) -> {
+//            StringBuilder theStrigBuilder = new StringBuilder(theText);
+//            return (theStrigBuilder.reverse().toString());
+//        });
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
