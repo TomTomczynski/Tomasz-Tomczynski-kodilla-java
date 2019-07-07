ebccaea0e484cdc837f336cf6f2e3ef20a127ea1
TomTomczynski <tom.tomczynski@gmail.com>
2019-07-07 17:50:17 +0200

    7.3 Zadanie: funkcjia spcer po liscie uzytkownikow forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -22,7 +22,6 @@ public class StreamMain {
         theResultForum.entrySet().stream()
                 .map(entry -> entry.getKey() + ":" + entry.getValue())
                 .forEach(System.out::println);
-
     }
 }
 
