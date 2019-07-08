8dcb863363185ea1901287a70ef8b0b672346ee3
TomTomczynski <tom.tomczynski@gmail.com>
2019-07-08 22:27:09 +0200

    7.4 Zadanie: obliczamy liosc ludzi na swiecie

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -20,5 +20,21 @@ public final class Country {
         return countryName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Country)) return false;
 
+        Country country = (Country) o;
+
+        if (!getCountryName().equals(country.getCountryName())) return false;
+        return getPeopleQuantity().equals(country.getPeopleQuantity());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getCountryName().hashCode();
+        result = 31 * result + getPeopleQuantity().hashCode();
+        return result;
+    }
 }
